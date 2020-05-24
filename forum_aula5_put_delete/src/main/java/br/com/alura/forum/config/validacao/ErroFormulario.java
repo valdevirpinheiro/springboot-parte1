ac574430746b25e851ca82e59eb686b47c50b895
valdevir <valdevir@hotmail.com>
2020-05-24 10:46:45 -0300

    first commit

---
+++ forum_aula5_put_delete/src/main/java/br/com/alura/forum/config/validacao/ErroFormulario.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.config.validacao;
+
+public class ErroFormulario {
+	private String campo;
+	private String mensagem;
+
+	public ErroFormulario(String campo, String mensagem) {
+		this.campo = campo;
+		this.mensagem = mensagem;
+	}
+	public String getCampo() {
+		return campo;
+	}
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
